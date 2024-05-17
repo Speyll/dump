c5167217d234f69f438510686c5cbdb1a284cad6
speyll <speyll@protonmail.com>
2024-05-17 01:35:21 +0100

    added some misc scripts

---
+++ scripts/shell/killGenshin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the Flatpak application name
+APP_NAME="moe.launcher.an-anime-game-launcher"
+
+# Find and kill bwarp processes associated with the specified Flatpak application
+bwarp_pids=$(pgrep -f "$APP_NAME")
+
+if [ -n "$bwarp_pids" ]; then
+    echo "Killing bwarp processes for $APP_NAME..."
+    for pid in $bwarp_pids; do
+        kill -9 "$pid"
+    done
+else
+    echo "No bwarp processes found for $APP_NAME"
+fi
+
